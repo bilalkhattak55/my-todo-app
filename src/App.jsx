abe2eb743d38a3d19859e1ac0b6525835158be0b
bilalkhattak95 <mbilalkhk@gmail.com>
2022-10-29 02:06:47 +0500

    my first todo app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import TodoList from './components/TodoList';
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
